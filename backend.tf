19de3e8533b4bdb50ea75f142615252d88fcde99
Akani Odomene <73500488+odomene@users.noreply.github.com>
2024-01-25 14:17:34 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
     bucket = "code-sharing-bucket"
     key = "state.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
     profile = "default"
     dynamodb_table = "firstprojecttable"
   }
